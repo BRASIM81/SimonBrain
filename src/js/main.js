2081595131da50a078618d5807ee9f340344aed7
Simon Brain <simonrbrain@yahoo.co.uk>
2020-03-30 17:47:23 +0100

    updated home link

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,44 @@
 new WOW().init();
 function myMap() {
 
-   var myCenter = new google.maps.LatLng(52.1010523, -1.9229738);
+	 var myCenter = new google.maps.LatLng(52.1010523, -1.9229738);
 
-   var mapProp = {
-       center: myCenter,
-       zoom: 16,
-       mapTypeId: google.maps.MapTypeId.HYBRID
-   };
+	 var mapProp = {
+			 center: myCenter,
+			 zoom: 16,
+			 mapTypeId: google.maps.MapTypeId.HYBRID
+	 };
 
-   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	 var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-   var marker = new google.maps.Marker({
-       position: myCenter,
-   });
+	 var marker = new google.maps.Marker({
+			 position: myCenter,
+	 });
 
-   marker.setMap(map);
+	 marker.setMap(map);
 }
 $(document).ready(function(){
-   var d = new Date();
-   var y = d.getFullYear();
-   document.getElementById('copy').innerHTML = "Simon Brain " + y;
+	 var d = new Date();
+	 var y = d.getFullYear();
+	 document.getElementById('copy').innerHTML = "Simon Brain " + y;
+	 $('.home-nav').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			 scrollTop: 0
+		},800);
+		});
+		$(window).on('scroll', function(){
+				var wScroll = $(this).scrollTop();
+				if(wScroll > $('.trigger').offset().top-($(window).height()/1.2)){
+				$('.home-nav').css({
+						'transform':'translate(0,0)',
+						'opacity':'0.8'
+				});
+				} else {
+				$('.home-nav').css({
+						'transform':'translate(-100px,0)',
+						'opacity':'0'
+				});
+				}
+		});
 });
